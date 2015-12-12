5f8b99b60872f0b052e30f175cf2d9dfcb71e1e8
Thomas Tuts <thomas@thomastuts.com>
2015-12-12 22:52:43 +0100

    Add initial implementation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './components/hello-world';
+
+ReactDOM.render(
+  <HelloWorld name="Captain Jack Sparrow" />,
+  document.getElementById('root')
+);
