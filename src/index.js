08f0ceb1b686bf220cbbf36e92b4ceb0a069d52b
Thomas Tuts <thomas@thomastuts.com>
2016-01-27 14:42:54 +0100

    Remove example component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import HelloWorld from './components/hello-world';
-
-ReactDOM.render(
-  <HelloWorld name="Captain Jack Sparrow" />,
-  document.getElementById('root')
-);
+// Code here!
